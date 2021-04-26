7de6cb1cd1bb3fd5b8c3d393ebdf2dcc0df93a90
jakelw96 <jake.lw96@gmail.com>
2021-04-25 23:20:25 -0600

    issue-4: Updated id of input. Change of project to a search for restaurants in an area. Need to try and get 401 to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,59 @@
 // Variables for different HTML elements
-var $inputCountry = document.getElementById(""); // Needs id for text area element
-var $currentTemp = document.getElementById(""); // Needs id for p element
+var $inputCity = document.getElementById("searchCity"); // Needs id for text area element
 
-// To get RoadGoat travel data
-var getTravelData = function(searchInput) {
-    var apiUrl = "https://Authorization: Basic access_key=f3f2954f0d07b3a20454de0bb4b93a2c:secret_key=69d8e4dab3ce3916431469b256014a9e@api.roadgoat.com/api/v2/destinations/auto_complete?q=" + 
-    searchInput; //+ //"&access_key=f3f2954f0d07b3a20454de0bb4b93a2c:secret_key=69d8e4dab3ce3916431469b256014a9e";
-    
-    //Make fetch request to url for travel data
-    fetch(apiUrl).then(function(data) {
-        //Request is successful
-        if (response.ok) {
-            response.json().then(function(data) {
-                console.log(data)
-            })
-        } else {
-            console.log("error")
-        };
-    });
-};
-//getTravelData("Utah");
 
-// To get weather data 
-var getWeatherData = function(lat, lon) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=&units=imperial&appid=6855f141fbc2348ef120b63c317a3472";
+// To get weather data and pass latitude and longitude
+var getWeatherData = function(citySearch) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=6855f141fbc2348ef120b63c317a3472";
 
     // Make fetch request to url for weather data
-    fetch(apiUrl).then(function(data) {
+    fetch(apiUrl).then(function(response) {
         // Request was sucessful
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data)
+                getRestaurantData(data)
             });
         } else {
-            console.log("error")
+            console.log("error") // Will need to connect to a modal
         };
     });
 };
+
+// To get data for restaurants
+var getRestaurantData = function(citySearch) {
+    //var lat = citySearch.coord.lat;
+    //var lon = citySearch.coord.lon;
+    //var $cuisineSearch = document.getElementById(""); // Needs id from cuisine input box
+    //var $mileageSearch = document.getElementById(""); // Needs id from mileage input box
+    
+    // Checks if there is data in all 3 fields or just the require field
+    //if (($cuisineSearch.value.trim()).length > 0) {
+        //var apiUrl = "https://api.documenu.com/v2/restaurants/search/geo?lat=" + lat + 
+        //"&lon=" + lon + "&distance=" + $mileageSearch.value.trim() + "&cuisine=" + 
+        //$cuisineSearch.value.trim() + "?key=b0aa35c300aa1872f312da8fd74be792";
+    //} else {
+        //var apiUrl = "https://api.documenu.com/v2/restaurants/search/geo?lat=" + lat + 
+        //"&lon=" + lon + "&distance=" + $mileageSearch.value.trim() + "?key=b0aa35c300aa1872f312da8fd74be792";
+    //}; 
+
+    //var apiUrl ="https://api.documenu.com/v2/restaurants/search/geo?lat=" + lat + 
+    //"&lon=" + lon + "&distance=" + 5 + "&fullmenu?key=b0aa35c300aa1872f312da8fd74be792";
+
+    // Search by zip code for restaurant data - need search box for zip code, cuisine, and mileage from zip code searched
+    var apiUrl = "https://api.documenu.com/v2/restaurants/zip_code/" + $inputCity.value + "?key=b0aa35c300aa1872f312da8fd74be792";
+
+    fetch(apiUrl).then(function(response) {
+        // Request sucessful
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        };
+    });
+};
+
+$(".btn").click(function() {
+    event.preventDefault();
+    getWeatherData($inputCity.value.trim());
+});
