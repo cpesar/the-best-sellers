2e0dab6bb5058431290df6490946ae278b650d9f
Geeva veeri <cbr_me@yahoo.com>
2021-04-26 20:17:27 -0600

    forgot to uncomment things

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,39 @@
-// // Variables for different HTML elements
-// var $inputCity = document.getElementById("searchCity"); // Needs id for text area element
+// Variables for different HTML elements
+var $inputCity = document.getElementById("searchCity"); // Needs id for text area element
 
 
-// // To get data for best selling books from NYTimes API 
-// var getBookData = function (citySearch) {
-//     var apiUrl = "https://api.nytimes.com/svc/books/v3/lists.json?list-name=" + citySearch + "&api-key=2njTMELLnHST5J4DsJ9Jc7ZeVO6TXVMc";
+// To get data for best selling books from NYTimes API 
+var getBookData = function (citySearch) {
+    var apiUrl = "https://api.nytimes.com/svc/books/v3/lists.json?list-name=" + citySearch + "&api-key=2njTMELLnHST5J4DsJ9Jc7ZeVO6TXVMc";
 
-//     // Make fetch request to url for weather data
-//     fetch(apiUrl).then(function (response) {
-//         // Request was sucessful
-//         if (response.ok) {
-//             response.json().then(function (data) {
-//                 console.log(data)
-//                 getImageData(data)
-//             });
-//         } else {
-//             console.log("error") // Will need to connect to a modal
-//         };
-//     });
-// };
+    // Make fetch request to url for weather data
+    fetch(apiUrl).then(function (response) {
+        // Request was sucessful
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data)
+                getImageData(data)
+            });
+        } else {
+            console.log("error") // Will need to connect to a modal
+        };
+    });
+};
 // getBookData("hardcover-fiction");
 
-// // To get images for best selling books from google books
-// var getImageData = function (bookData) {
-//     var bookId = bookData.results[0].isbns[0].isbn10;
-//     var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + bookId + "&key=AIzaSyBMxlyBiHn8m2_O63HOvZN-yqn-bAsJmFc";
+// To get images for best selling books from google books
+var getImageData = function (bookData) {
+    var bookId = bookData.results[0].isbns[0].isbn10;
+    var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + bookId + "&key=AIzaSyBMxlyBiHn8m2_O63HOvZN-yqn-bAsJmFc";
 
-//     fetch(apiUrl).then(function (response) {
-//         // Request sucessful
-//         if (response.ok) {
-//             response.json().then(function (data) {
-//                 console.log(data);
-//             });
-//         } else {
-//             console.log("error") // Will need to connect to a modal
-//         };
-//     });
-// };
+    fetch(apiUrl).then(function (response) {
+        // Request sucessful
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        } else {
+            console.log("error") // Will need to connect to a modal
+        };
+    });
+};
