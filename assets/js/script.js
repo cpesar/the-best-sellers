a49e6f1292c91cbdf15492e0eb3525762ce8564c
cpesar <77510555+cpesar@users.noreply.github.com>
2021-04-27 22:04:27 -0600

    for loop debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,6 @@ var getBookData = function() {
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data)
-                getImageData(data)
                 addToBookCards(data.results);
             });
         } else {
@@ -22,26 +21,47 @@ var getBookData = function() {
     });
 };
 
-function addToBookCards(results) {
+//FOR LOOP FOR AMAZON URL 
+async function addToBookCards(results) {
     for(var i = 0; i < 6; i++){
         var productUrl = results[i].amazon_product_url;
         console.log(productUrl);
+
+    document.getElementById('book-buy-' + i).setAttribute('href', productUrl);
+
+    
+    var imageThumbnail = await getImageData(results[0].isbns[0].isbn10);
+    console.log('image thumb: ',imageThumbnail);
+    document.getElementById('book-img-' + i).innerHTML = imageThumbnail;
+    
+
+    var bookDetails = results[i].book_details[0].description
+    document.getElementById('book-desc-' + i).innerHTML = bookDetails;
+
+
+    //UPDATE CSS OVERFLOW FOR BOOK IMAGE
+    var bookTitle = results[i].book_details[0].title
+    document.getElementById('book-title-' +i).innerHTML = bookTitle;
     }
+
 }
 
 
 // getBookData("hardcover-fiction");
-
 // To get images for best selling books from google books
-var getImageData = function (bookData) {
-    var bookId = bookData.results[0].isbns[0].isbn10;
+async function getImageData(bookId) {
+    
     var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + bookId + "&key=AIzaSyBMxlyBiHn8m2_O63HOvZN-yqn-bAsJmFc";
 
-    fetch(apiUrl).then(function (response) {
+    await fetch(apiUrl).then(function (response) {
         // Request sucessful
         if (response.ok) {
             response.json().then(function (data) {
-                console.log(data);
+                // console.log('data.thumbnail: ',data.items[0].volumeInfo.imageLinks.thumbnail);
+                if(!data.items) {
+                    return 'https://via.placeholder.com/128x180';
+                }
+                return(data.items[0].volumeInfo.imageLinks.thumbnail);
             });
         } else {
             $("#modal-title").text("Error: " + response.statusText);
