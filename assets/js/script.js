a5cd54701a38823ae6a649e2f5251c18450fe73f
Peter Montgomery <pmonty96@gmail.com>
2021-04-29 20:23:17 -0600

    issue-9: MVP for Saving Book Links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,29 @@
 // To get data for best selling books from NYTimes API 
+var booklist;
+
 var getBookData = function() {
     var search = document.getElementById("genre-choice").value;
     
     var apiUrl = "https://api.nytimes.com/svc/books/v3/lists.json?list-name=" + search + "&api-key=2njTMELLnHST5J4DsJ9Jc7ZeVO6TXVMc";
 
     // Make fetch request to url for weather data
-    fetch(apiUrl).then(function (response) {
+    booklist = fetch(apiUrl);
+    booklist.then(function (response) {
         // Request was sucessful
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data)
-                addToBookCards(data.results);
+                addToBookCards(data.results)
             });
         } else {
             $("#modal-title").text("Error: " + response.statusText);
             $(".modal").modal();
             $("#modal1").modal('open');
         };
-    });
+    });    
 };
+
+
 
 //FOR LOOP FOR AMAZON URL 
 function addToBookCards(results) {
@@ -39,7 +44,7 @@ function addToBookCards(results) {
     //UPDATE CSS OVERFLOW FOR BOOK IMAGE
     var bookTitle = results[i].book_details[0].title
     document.getElementById('book-title-' +i).innerHTML = bookTitle;
-    }
+    }    
 }
 
 
@@ -47,7 +52,7 @@ function addToBookCards(results) {
 // To get images for best selling books from google books
 function setImageData(bookId, index) {
     var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + bookId + "&key=AIzaSyBMxlyBiHn8m2_O63HOvZN-yqn-bAsJmFc";
-
+    
     fetch(apiUrl).then(function (response) {
         // Request sucessful
         if (response.ok) {
@@ -70,13 +75,47 @@ function setImageData(bookId, index) {
             $(".modal").modal();
             $("#modal1").modal('open');
         };
-    });
+    });    
 };
-
-
 
 //selector initializer
 $(document).ready(function(){
     $('select').formSelect();
   });
+
+//Save for Later
+var savednumber=localStorage.length;
+function saveForLater(amazonLink) {
+    savednumber++;
+    localStorage.setItem("saveBook"+savednumber, amazonLink);
+    
+    var savedList = document.getElementById("saved-book-links");
+    var listItem = document.createElement('li');
+    var listAnchor = document.createElement('a');
+    listAnchor.append(localStorage.getItem("saveBook"+savednumber, amazonLink));
+    listItem.appendChild(listAnchor);
+    savedList.appendChild(listItem);
+}
+writeToPage()
+//Write to Page
+function writeToPage() {
+    for (var i = 0; i < localStorage.length; i++){
+        var k = localStorage.key(i);
+        var v = localStorage.getItem(k);
+        console.log (v)
+    
+    var savedList = document.getElementById("saved-book-links");
+    var listItem = document.createElement('li');
+    var listAnchor = document.createElement('a');
+    let dataurl = encodeURI(v);
+    listAnchor.setAttribute('href', dataurl);
+    listAnchor.append(v);
+    listItem.appendChild(listAnchor);
+    savedList.appendChild(listItem);
+    }
+}
+
+function clearSaved() {
+    localStorage.clear()
+}
 
